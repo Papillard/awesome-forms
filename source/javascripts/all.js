c5bea886ac286c21631df5241cf18a8015d88e0a
Boris Paillard <boris.paillard@gmail.com>
2014-12-07 15:52:06 +0100

    Site updated to 4e712b7

--- source/javascripts/all.js
+++
@@ -1,6 +0,0 @@
-//= require jquery
-//= require bootstrap-sprockets
-//= require bootstrap-datepicker
-//= require bootstrap-datepicker/locales/bootstrap-datepicker.fr.js
-//= require selectize
-//= require_tree .
