35ee41368dfb474061e594f94111458f84480ce5
Boris Paillard <boris.paillard@gmail.com>
2014-12-07 18:52:53 +0100

    build

--- build/source/javascripts/all.js
+++
@@ -1,8 +0,0 @@
-//= require jquery
-//= require bootstrap-sprockets
-//= require jquery-ui
-//= require bootstrap-datepicker
-//= require bootstrap-datepicker/locales/bootstrap-datepicker.fr.js
-//= require selectize
-//= require jquery.nouislider
-//= require_tree .
